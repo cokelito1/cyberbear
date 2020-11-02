8b9cdd00e7aa09c7ce387a87c8121d3c43df6633
Jorge Bravo <cokelitocokelito@gmail.com>
2020-11-02 08:49:39 -0300

    Changed get_random_waifu()

--- helper.py
+++ helper.py
@@ -3,12 +3,13 @@ import random
 import json
 
 from waifu import WaifuData
+from context import Context
+
+ctx = Context()
 
 def get_random_image(path):
     return path + random.choice([x for x in os.listdir(path)
             if os.path.isfile(os.path.join(path, x))])
 
 def get_random_waifu_data():
-    with open(get_random_image("./waifus_data/")) as f:
-        tmp_data = json.load(f)
-        return WaifuData(tmp_data["nombre"], tmp_data["serie"], tmp_data["image_url"])
+    return random.choice(list(ctx.waifus.values()))
